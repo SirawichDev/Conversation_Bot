1a05e0bbca8ad83654d0f8df72e3709bd3cd16aa
SirawichDev <sirawit0676@gmail.com>
2018-03-17 10:28:56 +0700

    print&break test

--- chat_me.py
+++ chat_me.py
@@ -22,5 +22,13 @@ map_text_id = {}
 for line in lines:
     _line = line.split(' +++$+++ ')#_lineใช้เฉพาะใน loop
     if len(_line) == 5:
-        map_text_id[_line[0]] = _line[4] #map id กับ text
+        map_text_id[_line[0]] = _line[4]#map id กับ text
+      #  print(_line[4])
+      # print(map_text_id[_line[0]])
+      # break
         
+conver_split= ()
+for convers in conver:
+    _convers = convers.split(' +++$+++ ')[-1][1:-1].replace("'","").replace(" ","")
+    #print(_convers)
+    #break
